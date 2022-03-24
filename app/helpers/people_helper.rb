db3038e37fa6ede030ad0db773a06d91f2584cb9
nakamaksk <adebayanen@gmail.com>
2022-03-24 23:44:42 +0900

    change text color by fat level

--- app/helpers/people_helper.rb
+++ app/helpers/people_helper.rb
@@ -1,2 +1,13 @@
 module PeopleHelper
+  def fat_level(person)
+    class_name =
+      if person.fat_alert?
+        'text-danger'
+      elsif person.thin_alert?
+        'text-primary'
+      else
+        ''
+      end
+    tag.span person.fat_level, class: class_name
+  end
 end
--- app/models/person.rb
+++ app/models/person.rb
@@ -18,4 +18,29 @@ class Person < ActiveRecord::Base
   def bmi
     weight / (height_m)**2
   end
+
+  def fat_level
+    case bmi
+    when 0...18.5
+      '低体重'
+    when 18.5...25
+      '普通体重'
+    when 25...30
+      '肥満（１度）'
+    when 30...35
+      '肥満（２度）'
+    when 35...40
+      '肥満（３度）'
+    else
+      '肥満（４度）'
+    end
+  end
+
+  def fat_alert?
+    bmi >= 25
+  end
+
+  def thin_alert?
+    bmi < 18.5
+  end
 end
